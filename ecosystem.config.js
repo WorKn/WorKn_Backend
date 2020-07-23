dfcbe178ae66d11f4a362ed50676ea157d25b024
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-07-23 12:35:40 -0400

    PM2 Ecosystem updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     production: {
       user: "ubuntu",
       host: "ec2-3-133-94-54.us-east-2.compute.amazonaws.com/",
-      key: "~/.ssh/id_rsa.pem",
+      key: "~/.ssh/id_rsa",
       ref: "origin/master",
       repo: "git@github.com:WorKn/WorKn_Backend.git",
       path: "/home/ubuntu/WorKn_Backend",
