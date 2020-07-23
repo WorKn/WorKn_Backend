fa95abbe911de97a25e05c9c5851e826cb251b6d
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-07-23 15:08:07 -0400

    Bug fiexd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors);
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.send("Hello WorKn!");
