e143d60f838aad686c4a0969fdcb6caae90b9f3b
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-07-30 12:07:23 -0400

    index.js removed

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 //---Global Middlewares---
 
+//CORS
+app.use(cors());
+
 //Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 //---Routes---
+
+//Temporal endpoint
+app.get('/', function (req, res) {
+  res.send('Hello WorKn!!!');
+});
 
 //---Error handling---
 
