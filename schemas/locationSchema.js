78cab9132e79896f3ffb2b4e9c7e9d3cee83c444
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-08-02 19:47:59 -0400

    locationSchema id set to false

--- schemas/locationSchema.js
+++ schemas/locationSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const locationSchema = new mongoose.Schema({
   coordinates: [Number],
   address: String,
+  _id: false,
 });
 
 module.exports = locationSchema;
