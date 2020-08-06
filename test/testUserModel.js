bbeae1b1670cc2ed2144d5fd0bd932b242615df1
dreinix <ricardo.aar@outlook.com>
2020-08-05 21:12:37 -0400

    Fixed merge problem

--- test/testUserModel.js
+++ test/testUserModel.js
@@ -4,53 +4,53 @@ const dbConection = require('./dbConection');
 const testModel = require('./testModel');
 
 new_element = {
-  name: 'Ricardo Arias',
-  email: 'yonose@gmail.com',
+  name: 'Yordi Ogando',
+  email: 'balbla4325@gmail.com',
+  identificationNumber: '12345678910',
+  password: '123456878',
+  passwordConfirm: '123456878',
+  userType: 'offerer',
+  // userType: 'applicant',
+  birthday: '2003-09-12',
+  // category: 'Tecnologa',
+  location: {
+    coordinates: [13.25, 25.002],
+    address: 'calle 4',
+  },
+  // tags: [
+  //   {
+  //     name: 'tag1',
+  //     category: { name: 'category1' },
+  //   },
+  //   {
+  //     name: 'tag2',
+  //     category: { name: 'category2' },
+  //   },
+  // ],
+};
+
+new_element2 = {
+  name: 'Yordi Ogando',
+  email: 'balbla@gmail2.com',
   identificationNumber: '12345678910',
   password: '123456878',
   passwordConfirm: '123456878',
   // userType: 'offerer',
   userType: 'applicant',
-  birthday: '2001-12-12',
-  category: '5f2375264620f36c4ba15a7c',
+  birthday: '2003-09-12',
+  category: 'Tecnologa',
   location: {
     coordinates: [13.25, 25.002],
     address: 'calle 4',
   },
   tags: [
+    // {
+    //   name: 'tag1',
+    //   category: { name: 'category1' },
+    // },
     {
       name: 'tag1',
       category: { name: 'category1' },
-    },
-    {
-      name: 'tag2',
-      category: { name: 'category2' },
-    },
-  ],
-};
-
-new_element = {
-  name: 'Ricardo Arias ofertante',
-  email: 'yonosenada@gmail.com',
-  identificationNumber: '12345675210',
-  password: '123456878',
-  passwordConfirm: '123456878',
-  userType: 'offerer',
-  //userType: 'applicant',
-  birthday: '2001-12-12',
-  category: '5f2375264620f36c4ba15a7c',
-  location: {
-    coordinates: [13.25, 25.002],
-    address: 'calle 4',
-  },
-  tags: [
-    {
-      name: 'tag1',
-      category: { name: 'category1' },
-    },
-    {
-      name: 'tag2',
-      category: { name: 'category2' },
     },
   ],
 };
