d69780897c39ba89dbfe3fe70d482e538d5080c3
dreinix <ricardo.aar@outlook.com>
2020-08-05 21:06:14 -0400

    payment log module created

---
+++ models/paymentLogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const paymentLogSchema = mongoose.Schema({
+    organization: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Organization',
+        required: [true, "Un pago debe ser emitido por una organización"]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    amount:{
+        type: Number,
+        required: [true, "Un pago debe contener el monto"],
+    },
+})
