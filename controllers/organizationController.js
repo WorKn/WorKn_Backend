fc5ecdbfd6ce7b8e597e597da4ed9014adeeb81e
dreinix <ricardo.aar@outlook.com>
2020-08-06 22:35:09 -0400

    added require

---
+++ controllers/organizationController.js
@@ -0,0 +1,5 @@
+const Organization = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+
