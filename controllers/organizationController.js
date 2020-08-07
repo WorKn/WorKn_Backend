506882483a6d35320f9972dae2959bf53c290d34
dreinix <ricardo.aar@outlook.com>
2020-08-07 18:42:17 -0400

    solved consistency issue

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -3,13 +3,13 @@ const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.organization = catchAsync(async (req, res, next) => {
+exports.createOrganization = catchAsync(async (req, res, next) => {
     
     if(req.user.organization){
         return next(new AppError("Usted ya posee una organización asociada",400));
     }
         
-    const newOrganization = await Organization.create({
+    const organization = await Organization.create({
       name: req.body.name,
       RNC: req.body.RNC,
       location: req.body.location,
--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createOrganization } = require('./../controllers/organizationController');
-const { restricTo , protect } = require('./../controllers/authController');
+const { restricTo , protect} = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/createOrganization', protect ,restricTo('owner') ,createOrganization);
+router.post('/createOrganization' , protect, restricTo('owner') ,createOrganization);
+
 
 module.exports = router;
