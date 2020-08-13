16f69c380c0dc80560c93691dbb36c4ddd2e112c
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-08-13 11:21:46 -0400

    Preliminar updateMyProfile endpoint finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('./../models/userModel');
+const Tag = require('./../models/tagModel');
 const factory = require('./handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
@@ -25,21 +26,17 @@ exports.updateMyProfile = catchAsync(async (req, res, next) => {
     );
   }
 
-  //Filter out unwanted fields names that are not allowed to be updated
   let allowedFields = ['name', 'identificationNumber', 'phone', 'location'];
 
-  if (req.user.userType === 'applicant') allowedFields.push('category', 'tags');
+  if (req.user.userType === 'applicant') {
+    allowedFields.push('category', 'tags');
 
-  //   const filteredBody = filterObj(
-  //     req.body,
-  //     'name',
-  //     'identificationNumber',
-  //     'phone',
-  //     'category',
-  //     'location',
-  //     'tags'
-  //   );
-  const filteredBody = filterObj(req.body, allowedFields);
+    const tags = await Tag.find({ _id: { $in: req.body.tags } }).select('-_id -__v');
+    req.body.tags = tags;
+  }
+
+  //Filter out unwanted fields names that are not allowed to be updated
+  let filteredBody = filterObj(req.body, allowedFields);
 
   // Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- schemas/sharedCategorySchema.js
+++ schemas/sharedCategorySchema.js
@@ -6,6 +6,7 @@ const categorySchema = mongoose.Schema({
     require: true,
     lowercase: true,
   },
+  _id: false,
 });
 
 module.exports = categorySchema;
