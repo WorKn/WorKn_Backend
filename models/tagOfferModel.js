28123c8bc6e633c170094762c49d30c30b2077d7
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-08-14 01:03:47 -0400

    Added unique validation for multiple fields in tagUser and tagOffer models

--- models/tagOfferModel.js
+++ models/tagOfferModel.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
 const tagOfferSchema = mongoose.Schema({
-    tag:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tag',
-        required: [true,"Debe de estar asociado a un tag."]
-    },
-    offer:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Offer',
-        required: [true,"Debe de estar asociado a una oferta."]
-    }
-})
+  tag: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tag',
+    required: [true, 'Debe de estar asociado a un tag.'],
+    index: true,
+  },
+  offer: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Offer',
+    required: [true, 'Debe de estar asociado a una oferta.'],
+    index: true,
+  },
+});
 
-const tagOffer = mongoose.model('TagOffer',tagOfferSchema);
+tagUserSchema.index({ tag: 1, offer: 1 }, { unique: true });
+
+const tagOffer = mongoose.model('TagOffer', tagOfferSchema);
 
 module.exports = tagOffer;
--- models/tagUserModel.js
+++ models/tagUserModel.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
 const tagUserSchema = mongoose.Schema({
-    tag:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tag',
-        required: [true,"Debe de estar asociado a un tag"]
-    },
-    user:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true,"Debe de estar asociado a un usuario"]
-    }
-})
+  tag: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tag',
+    required: [true, 'Debe de estar asociado a un tag'],
+    index: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Debe de estar asociado a un usuario'],
+    index: true,
+  },
+});
 
-const tagUser = mongoose.model('TagUser',tagUserSchema);
+tagUserSchema.index({ tag: 1, user: 1 }, { unique: true });
+
+const tagUser = mongoose.model('TagUser', tagUserSchema);
 
 module.exports = tagUser;
