dd0be32841fd6d6dab4ec5dfd8b8488286a6093e
Yordi Ogando <yordi.ogando@pitech.com.do>
2020-08-16 19:44:22 -0400

    Removed unncesary code

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -5,25 +5,16 @@ const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 const filterObj = (obj, allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach((el) => {
-      if (allowedFields.includes(el)) newObj[el] = obj[el];
-    });
-    return newObj;
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
 };
 
 exports.createOrganization = catchAsync(async (req, res, next) => {
   if (req.user.organization) {
     return next(new AppError('Usted ya posee una organización asociada.', 400));
-  }
-
-  if (req.user.organizationRole != 'owner') {
-    return next(
-      new AppError(
-        'Usted no puede crear una organización. Por favor, realice el proceso de registro correspondiente.',
-        400
-      )
-    );
   }
 
   const organization = await Organization.create({
@@ -45,37 +36,43 @@ exports.createOrganization = catchAsync(async (req, res, next) => {
     },
   });
 });
-exports.editOrganization = catchAsync(async(req,res,next) => {
+exports.editOrganization = catchAsync(async (req, res, next) => {
+  if (req.user.organization != req.params.id) {
+    return next(
+      new AppError(
+        'Usted no pertenece a esta organización, no tiene permisos para editarla',
+        401
+      )
+    );
+  }
+  if (req.body.members) {
+    return next(
+      new AppError(
+        'No puedes modificar tus miembros aquí, por favor, dirígase al menú de miembros',
+        400
+      )
+    );
+  }
 
-    if(req.user.organization != req.params.id){
-        return next(
-            new AppError("Usted no pertenece a esta organización, no tiene permisos para editarla",401));
-    }
-    if (req.body.members) {
-        return next( new AppError(
-            'No puedes modificar tus miembros aquí, por favor, dirígase al menú de miembros', 400)
-        );
-      }
-      
-    allowedFields = ['name', 'location', 'phone', 'email'];
-    
-    org = await Organization.findById(req.params.id);
-    if(!org.RNC) {
-        allowedFields.push("RNC");
-    }      
-    filteredBody = filterObj(req.body, allowedFields);
-    
-    const updatedOrg = await Organization.findByIdAndUpdate(req.params.id, filteredBody, {
-        new: true,
-        runValidators: true
-    });
+  allowedFields = ['name', 'location', 'phone', 'email'];
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-          organization: updatedOrg,
-        },
-      });
+  org = await Organization.findById(req.params.id);
+  if (!org.RNC) {
+    allowedFields.push('RNC');
+  }
+  filteredBody = filterObj(req.body, allowedFields);
+
+  const updatedOrg = await Organization.findByIdAndUpdate(req.params.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      organization: updatedOrg,
+    },
+  });
 });
 exports.getOrganization = factory.getOne(Organization);
 
