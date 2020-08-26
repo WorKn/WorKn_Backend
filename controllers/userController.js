8b57a617a576131ae387a4d0fa081550c59c8d0f
Yordi Ogando <yordiogando@gmail.com>
2020-08-26 16:35:27 -0400

    Refactor updateTags to a separated file

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const factory = require('./handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const filterObj = require('./../utils/filterObj');
+const updateTags = require('./../utils/updateTags');
 
 const User = require('./../models/userModel');
 const Tag = require('./../models/tagModel');
@@ -49,9 +50,8 @@ exports.updateMyProfile = catchAsync(async (req, res, next) => {
 
   updatedUser.save({ validateBeforeSave: false });
 
-  //Create the new TagUser records asynchronously
-  // if (req.user.userType === 'applicant' && req.user.tags) updateTagUser(req.user.id, tagsRef);
-  if (req.user.userType === 'applicant' && tagsRef) updateTags(req.user, tagsRef);
+  //Update TagUser records asynchronously
+  if (req.user.userType === 'applicant' && tagsRef) updateTags(req.user, tagsRef, TagUser);
 
   res.status(200).json({
     status: 'success',
@@ -60,55 +60,3 @@ exports.updateMyProfile = catchAsync(async (req, res, next) => {
     },
   });
 });
-//TODO: Export this functions to a separate module and use it in Offers controller
-const updateTags = async (user, tags) => {
-  let userTags = [];
-  if (user.tags) {
-    user.tags.forEach((tag) => userTags.push(tag.id));
-  }
-
-  const currentTags = new Set(userTags);
-  const newTags = new Set(tags);
-
-  const tagsToBeAdded = difference(newTags, currentTags);
-  const tagsToBeDeleted = difference(currentTags, newTags);
-
-  addTagUser(user.id, tagsToBeAdded);
-  deleteTagUser(user.id, tagsToBeDeleted);
-};
-
-const difference = (a, b) => {
-  return new Set([...a].filter((x) => !b.has(x)));
-};
-
-const addTagUser = async (user, tags) => {
-  tags.forEach((tag) => {
-    TagUser.create({ tag, user }).catch((err) => {
-      //Error code 11000 = Duplicate key
-      if (err.code != 11000) console.log(err);
-      // else if (!err) console.log('SUCCESS: tag(', tag, ') created');
-    });
-  });
-};
-
-const deleteTagUser = async (user, tags) => {
-  tags.forEach((tag) => {
-    TagUser.deleteOne({ tag, user }).catch((err) => {
-      if (err) {
-        console.log('ERROR:\n', err);
-      }
-      // else {
-      //   console.log('SUCCESS: tag(', tag, ') deleted');
-      // }
-    });
-  });
-};
-
-const updateTagUser = async (user, tags) => {
-  tags.forEach((tag) => {
-    TagUser.create({ tag, user }).catch((err) => {
-      //Error code 11000 = Duplicate key
-      if (err.code != 11000) console.log(err);
-    });
-  });
-};
