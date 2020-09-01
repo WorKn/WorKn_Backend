758a1a596dae687b280ccb28e9f11d49c5b8b15b
dreinix <ricardo.aar@outlook.com>
2020-08-31 20:11:59 -0400

    added getCategoriesTag

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,20 @@
 const Category = require('./../models/categoryModel');
 const factory = require('./handlerFactory');
+const Tag = require('../models/tagModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
 
 exports.getAllCategories = factory.getAll(Category);
+
+exports.getCategoriesTag = catchAsync(async(req,res,next)=>{
+    var categoryID = (await Category.findOne({name: req.params.categoryName}))
+    var tags = await Tag.find( {category: categoryID} );
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tags: tags,
+        },  
+    });
+}); 
+   
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {
-    getAllCategories
+    getAllCategories,
+    getTagOnCategory
 } = require('./../controllers/categoryController');
 const router = express.Router();
 
 router.get('/',getAllCategories);
+router.get('/:categoryName/tags',getTagOnCategory);
 
 module.exports = router;
