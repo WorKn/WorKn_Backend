0b2739f68c1d6523cad756cc8867324db7f1c9b6
dreinix <ricardo.aar@outlook.com>
2020-09-02 17:06:50 -0400

    added some filter for email validation

---
+++ controllers/interactionController.js
@@ -0,0 +1,7 @@
+const Interaction = require('./../models/InteractionModel');
+const factory = require('./handlerFactory');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.createInteraction = catchAsync( async(req,res,next) =>{
+    
+});
