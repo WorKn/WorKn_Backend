fec3fe224b6031c155156c6c43000e111d683471
dreinix <ricardo.aar@outlook.com>
2020-09-02 23:24:15 -0400

    removed interactionRoutes

--- routes/offerRoutes.js
+++ routes/offerRoutes.js
@@ -12,14 +12,19 @@ const {
   protect,
   verifyEmailValidation,
 } = require('./../controllers/authController');
-
+const { createInteraction } = require('./../controllers/interactionController');
 const router = express.Router();
 
 router.get('/', getAllOffers);
+
 router.get('/:id', getOffer);
 
 //Protected routes
-router.use(protect, restricTo('offerer'), verifyEmailValidation);
+router.use(protect, verifyEmailValidation);
+
+router.route('/interaction').post(createInteraction);
+
+router.use(restricTo('offerer'));
 
 router.post('/', createOffer);
 
