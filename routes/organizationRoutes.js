cd3859c7850e7b96980d20f5e56f6e72ed23d75b
Yordi Ogando <yordiogando@gmail.com>
2020-09-02 21:34:49 -0400

    Fixed editOrganization route, to PATCH

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { 
+const {
   createOrganization,
   getAllOrganizations,
   getOrganization,
@@ -9,9 +9,9 @@ const {
   addOrganizationMember,
   validateMemberInvitation,
   updateMemberRole,
-  removeOrganizationMember
+  removeOrganizationMember,
 } = require('./../controllers/organizationController');
-const { restricTo , protect} = require('./../controllers/authController');
+const { restricTo, protect } = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -19,26 +19,22 @@ router.get('/', getAllOrganizations);
 router.get('/myOrganization', protect, getMyOrganization, getOrganization);
 router.get('/:id', getOrganization);
 
-router
-  .route('/:id/:token')
-  .get(protect,validateMemberInvitation,getOrganization); 
+router.route('/:id/:token').get(protect, validateMemberInvitation, getOrganization);
 
 //Protected routes
 router.use(protect, restricTo('owner'));
 
 router.post('/', createOrganization);
-router.post('/:id', editOrganization);
+router.patch('/:id', editOrganization);
 router
   .route('/:id/members/invite')
-  .post(restricTo("owner", "supervisor"),sendInvitationEmail);
+  .post(restricTo('owner', 'supervisor'), sendInvitationEmail);
 router
   .route('/:id/members')
-  .get(restricTo("owner","supervisor","member"),getOrganization)
-  .post(restricTo("supervisor","owner"),updateMemberRole)
-  .delete(restricTo("supervisor","owner"),removeOrganizationMember);
-  
-router
-  .route('/:id/members/add')
-  .post(restricTo("supervisor","owner"),addOrganizationMember);
+  .get(restricTo('owner', 'supervisor', 'member'), getOrganization)
+  .post(restricTo('supervisor', 'owner'), updateMemberRole)
+  .delete(restricTo('supervisor', 'owner'), removeOrganizationMember);
+
+router.route('/:id/members/add').post(restricTo('supervisor', 'owner'), addOrganizationMember);
 
 module.exports = router;
