4c4c4aaf20df70ab8c8181c4b5ef8ef67d037b21
Yordi Ogando <yordiogando@gmail.com>
2020-09-03 23:28:20 -0400

    Code refactor and getMy Interactions initial commit

---
+++ routes/interactionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { restricTo, protect, verifyEmailValidation } = require('../controllers/authController');
+const {
+  createInteraction,
+  getMyInteractions,
+  protectOfferInteraction,
+} = require('../controllers/interactionController');
+const router = express.Router();
+
+//Protected routes
+router.use(protect, verifyEmailValidation);
+
+router.route('/').post(protectOfferInteraction, createInteraction);
+
+router.route('/me').get(getMyInteractions);
+
+module.exports = router;
