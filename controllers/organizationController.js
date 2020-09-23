02d16be515adb30d99353084618b4832a50e088b
Yordi Ogando <yordiogando@gmail.com>
2020-09-23 16:09:55 -0400

    Added bio field to editOrganization endpoint

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -11,7 +11,7 @@ const getClientHost = require('./../utils/getClientHost');
 const jwt = require('jsonwebtoken');
 
 exports.protectOrganization = catchAsync(async (req, res, next) => {
-  if(req.user.userType =="applicant"){
+  if (req.user.userType == 'applicant') {
     return next(
       new AppError(
         'Usted no pertenece a esta organización, por favor, contacte con un supervisor o con el dueño de la organización.',
@@ -20,7 +20,7 @@ exports.protectOrganization = catchAsync(async (req, res, next) => {
     );
   }
   const org = await Organization.findById(req.user.organization).select('+members');
-  if(!org){
+  if (!org) {
     return next(new AppError('No se ha podido encontrar la organización especificada', 404));
   }
   if (!org.members.includes(req.user.id)) {
@@ -71,7 +71,7 @@ exports.editOrganization = catchAsync(async (req, res, next) => {
     );
   }
 
-  allowedFields = ['name', 'location', 'phone', 'email'];
+  allowedFields = ['name', 'bio', 'location', 'phone', 'email'];
   if (req.organization.RNC) {
     allowedFields.push('RNC');
   }
@@ -104,7 +104,7 @@ exports.getAllOrganizations = factory.getAll(Organization);
 // Members-----------------
 
 exports.addOrganizationMember = catchAsync(async (req, res, next) => {
-  req.organization =  await Organization.findById(req.user.organization).select('+members');
+  req.organization = await Organization.findById(req.user.organization).select('+members');
   if (!req.organization.members.includes(req.user.id)) {
     if (req.user.organization != req.organization.id) {
       return next(
@@ -115,7 +115,7 @@ exports.addOrganizationMember = catchAsync(async (req, res, next) => {
       );
     }
     req.organization.members.push(req.user);
-  }else{
+  } else {
     return next(
       new AppError(
         'Usted ya es un miembro de la aplicación, no se puede volver a agregar.',
@@ -124,12 +124,12 @@ exports.addOrganizationMember = catchAsync(async (req, res, next) => {
     );
   }
 
-  req.organization.save({validateBeforeSave: false});
+  req.organization.save({ validateBeforeSave: false });
 
   res.status(201).json({
     status: 'successs',
     data: {
-      user: req.user
+      user: req.user,
     },
   });
 });
@@ -166,7 +166,7 @@ exports.updateMemberRole = catchAsync(async (req, res, next) => {
 });
 exports.removeOrganizationMember = catchAsync(async (req, res, next) => {
   const member = await User.findById(req.params.id);
-  if(!member){
+  if (!member) {
     return next(new AppError('Este usuario no existe', 400));
   }
   if (!req.organization.members.includes(member.id)) {
@@ -195,13 +195,17 @@ exports.removeOrganizationMember = catchAsync(async (req, res, next) => {
   member.organizationRole = undefined;
   member.organization = undefined;
   member.isActive = false;
-  member.email = member.email+Math.random();
+  member.email = member.email + Math.random();
   member.save({ validateBeforeSave: false });
 
-  const organization = await Organization.findByIdAndUpdate(req.organization.id, req.organization, {
-    new: true,
-    runValidators: true,
-  }).select('+members');
+  const organization = await Organization.findByIdAndUpdate(
+    req.organization.id,
+    req.organization,
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).select('+members');
   organization.save({ validateBeforeSave: false });
 
   res.status(201).json({
@@ -211,7 +215,6 @@ exports.removeOrganizationMember = catchAsync(async (req, res, next) => {
     },
   });
 });
-
 
 //Invitations--------------------
 
@@ -285,7 +288,6 @@ exports.sendInvitationEmail = catchAsync(async (req, res, next) => {
   });
 });
 
-
 exports.validateMemberInvitation = catchAsync(async (req, res, next) => {
   encryptedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
   var invitation = await MemberInvitation.findOne({
@@ -300,32 +302,31 @@ exports.validateMemberInvitation = catchAsync(async (req, res, next) => {
   return next(new AppError('Token inválido, acceso denegado.', 403));
 });
 
-exports.getInvitationInfo = catchAsync(async(req,res,next)=>{
+exports.getInvitationInfo = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      message: "access authorized",
+      message: 'access authorized',
       organization: req.organization,
       email: req.invitedEmail,
-      invitedRole: req.invitedRole
+      invitedRole: req.invitedRole,
     },
   });
-})
+});
 
-exports.deleteInvitation=catchAsync(async(req,res,next)=>{
+exports.deleteInvitation = catchAsync(async (req, res, next) => {
   let encryptedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
   await MemberInvitation.deleteOne({
     token: encryptedToken,
   });
   console.log(`The invitation to ${req.user.email} was deleted`);
   next();
-})
+});
 
 // Register------------------
 
 exports.signupOrganizationMember = catchAsync(async (req, res, next) => {
-
-  try{
+  try {
     const newUser = await User.create({
       name: req.body.name,
       lastname: req.body.lastname,
@@ -337,7 +338,7 @@ exports.signupOrganizationMember = catchAsync(async (req, res, next) => {
       organizationRole: req.invitedRole,
       organization: req.organization.id,
     });
-    
+
     await newUser.save({ validateBeforeSave: false });
     createSendToken(newUser, res);
 
@@ -345,11 +346,12 @@ exports.signupOrganizationMember = catchAsync(async (req, res, next) => {
 
     req.user = newUser;
     next();
-  }catch(err){
-    await User.deleteOne({email: req.invitedEmail});
-    return next(new AppError('Algo salió mal al crear su cuenta, por favor, intente de nuevo',500))
+  } catch (err) {
+    await User.deleteOne({ email: req.invitedEmail });
+    return next(
+      new AppError('Algo salió mal al crear su cuenta, por favor, intente de nuevo', 500)
+    );
   }
-  
 });
 
 const signToken = (id) => {
@@ -375,4 +377,3 @@ const createSendToken = (user, res) => {
   res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
 };
-
