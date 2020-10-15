07bfd4420b454de86347c280ab5584130a615d2e
Yordi Ogando <yordiogando@gmail.com>
2020-10-15 16:29:03 -0400

    updated validateCreateReview middleware

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('./../models/reviewModel');
 const User = require('./../models/userModel');
+const Interaction = require('./../models/InteractionModel');
 
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
@@ -23,8 +24,55 @@ exports.validateCreateReview = catchAsync(async (req, res, next) => {
     return next(new AppError('Usuario no encontrado.', 404));
   }
 
+  if (userReviewed.userType === req.user.userType) {
+    return next(new AppError('No puede hacer un review a un usuario de su mismo tipo.', 400));
+  }
+
+  if (userReviewed.organization) {
+    return next(new AppError('No puede hacer review de una organización.', 403));
+  }
+
+  let interactions = [];
+
+  if (req.user.userType == 'offerer') {
+    interactions = Interaction.find({
+      state: 'match',
+      applicant: userReviewed.id,
+      offerer: req.user.id,
+    });
+  } else if (req.user.userType == 'applicant') {
+    interactions = Interaction.find({
+      state: 'match',
+      offerer: userReviewed.id,
+      applicant: req.user.id,
+    });
+  }
+
+  interactions = await interactions.populate({
+    path: 'offer',
+    match: { offerType: 'free' },
+  });
+
+  if (interactions.length == 0 || areAllOffersNull(interactions)) {
+    return next(
+      new AppError(
+        'No reune las condiciones necesarias para crear un review al usuario indicado.',
+        403
+      )
+    );
+  }
+
   next();
 });
+
+const areAllOffersNull = (interactions) => {
+  let output = true;
+  interactions.forEach((interaction) => {
+    if (interaction.offer) output = false;
+  });
+
+  return output;
+};
 
 exports.setUsersIds = (req, res, next) => {
   req.body.userReviewed = req.params.userId;
